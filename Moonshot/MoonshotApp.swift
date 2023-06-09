4304cbc8923e426274534fb4895f08d26037cbc8
Blair Duddy <blairduddy@hotmail.co.uk>
2023-06-09 15:14:11 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Blair Duddy on 2023-06-09.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
